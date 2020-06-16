cdc96b24f91ccdf45577ab710cb30001d9755808
Flavien-htz <flavien.htz@gmail.com>
2020-06-16 16:09:51 +0200

    fixtures users (encodage) probleme git regler

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,22 +5,28 @@ namespace App\DataFixtures;
 use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    
+    private $password;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->password = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
 
-        $faker = Faker\Factory::create('fr_FR');
-
         $user = new Users();
 
-        $user->setUsername($faker->word())
-            ->setPassword(
-            $faker->word()
-            );
+        $user->setUsername('demo')
+            ->setPassword($this->password->encodePassword($user, 'demo'))
+            ->setRoles([
+                'ROLE_ADMIN'
+            ]);
 
         $manager->persist($user);
 
